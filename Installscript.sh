2fd60d9b7fb524ddcdad6abd8bb744f73b470076
dungphung411 <mdungphung411@gmail.com>
2024-09-13 14:29:24 +0700

    oke

--- Installscript.sh
+++ Installscript.sh
@@ -6,7 +6,7 @@ curl -fsSL https://deb.nodesource.com/setup_20.x | sudo -E bash -
 
 sudo -S apt install -y nodejs
   
- sudo npm install -g @angular/cli -y   #chau fix dc loi bat buoc phai chay root de install ( have to manual install using root )
+sudo npm install -g @angular/cli -y   #chau fix dc loi bat buoc phai chay root de install ( have to manual install using root )
 
 sudo -y apt autoremove
 
