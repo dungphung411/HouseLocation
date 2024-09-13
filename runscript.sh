0264839883f3a094fde0bcbf55404f329225bc11
dungphung411 <mdungphung411@gmail.com>
2024-09-13 16:15:34 +0700

    oke

--- runscript.sh
+++ runscript.sh
@@ -1,3 +1,3 @@
 # maybe need to chmod +x runscript.sh first to avoid permission denied 
 
-start json-server --watch db.json & ng serve --host 0.0.0.0
+ json-server --watch db.json & ng serve --host 0.0.0.0
